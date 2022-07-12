4d3ddeb7d730f8cd2e92d34e49dfdb697712d97b
Gilberto Sanchez Mares <69218351+GSanchezMTD@users.noreply.github.com>
2022-07-12 16:19:02 -0500

    Update seed1.groovy

--- seed1.groovy
+++ seed1.groovy
@@ -2,8 +2,10 @@ def githubUrl = "https://github.com/m-goos/jenkins-jobdsl-seedrepo-example.git"
 
 pipelineJob("Seed job 1") {
 definition {
-    stage('UIUX - Search - LG5-5254') {
-        sh 'COMPONENT=MT/Homepage/UIUX TESTCASE=SmokeTestSearch npm run devtools-kubernetes'
+    node('build') {
+        stage('UIUX - Search - LG5-5254') {
+            sh 'COMPONENT=MT/Homepage/UIUX TESTCASE=SmokeTestSearch npm run devtools-kubernetes'
+        }
     }
 }
 }
