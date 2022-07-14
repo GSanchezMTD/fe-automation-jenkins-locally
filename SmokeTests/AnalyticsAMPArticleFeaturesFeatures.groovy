cbded7f3f35f3e22e55002357b1a4c93fa266103
GSanchezMTD <gsanchezm@tiempodevelopment.com>
2022-07-14 16:52:41 -0500

    Experiments

--- SmokeTests/AnalyticsAMPArticleFeaturesFeatures.groovy
+++ SmokeTests/AnalyticsAMPArticleFeaturesFeatures.groovy
@@ -1,12 +1,13 @@
 def path1 = "ONO/Front End/FE Smoke Tests - Staging/";
-
+def path2 = "ONO/Front End/FE Smoke Tests - Preproduction behind Akamai";
+def path3 = "ONO/Front End/FE Smoke Tests - Production/";
 def component = "MT/AMPArticles/Analytics/Features";
 def testcase = "SmokeTestMTFeaturesAMPArticle";
 def server1 = "carbon-stg";
 def server2 = "carbon-preprod-akamai";
 def server3 = "prod";
 
-pipelineJob(${path1} + "Analytics - AMP Article - Features - Features - LG5-5201") {
+pipelineJob("${path1}" + "Analytics - AMP Article - Features - Features - LG5-5201") {
   definition {
     cps {
       script('''
@@ -15,43 +16,7 @@ pipelineJob(${path1} + "Analytics - AMP Article - Features - Features - LG5-5201
           stages {
             stage('Build'){
                 steps{
-                    sh 'COMPONENT='''+${component}+''' TESTCASE='''+${testcase}+''' SERVER='''+${server1}+''' npm run devtools-kubernetes'
-                }
-            }
-          }
-        }
-    ''')
-    }
-  }
-}
-pipelineJob("ONO/Front End/FE Smoke Tests - Preproduction behind Akamai/Analytics - AMP Article - Features - Features - LG5-5201") {
-  definition {
-    cps {
-      script('''
-        pipeline {
-          agent any
-          stages {
-            stage('Build'){
-                steps{
-                    sh 'COMPONENT='''+${component}+''' TESTCASE='''+${testcase}+''' SERVER='''+${server1}+''' npm run devtools-kubernetes'
-                }
-            }
-          }
-        }
-    ''')
-    }
-  }
-}
-pipelineJob("ONO/Front End/FE Smoke Tests - Production/Analytics - AMP Article - Features - Features - LG5-5201") {
-  definition {
-    cps {
-      script('''
-        pipeline {
-          agent any
-          stages {
-            stage('Build'){
-                steps{
-                    sh 'COMPONENT='''+${component}+''' TESTCASE='''+${testcase}+''' SERVER='''+${server1}+''' npm run devtools-kubernetes'
+                    sh 'COMPONENT='''+"${component}"+''' TESTCASE='''+"${testcase}"+''' SERVER='''+"${server1}"+''' npm run devtools-kubernetes'
                 }
             }
           }
