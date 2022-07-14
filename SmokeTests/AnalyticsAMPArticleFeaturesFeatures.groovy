c44f2e74ee43288682d92a34378fa3b7bd55a957
GSanchezMTD <gsanchezm@tiempodevelopment.com>
2022-07-14 16:37:02 -0500

    Experiments

--- SmokeTests/AnalyticsAMPArticleFeaturesFeatures.groovy
+++ SmokeTests/AnalyticsAMPArticleFeaturesFeatures.groovy
@@ -16,3 +16,39 @@ pipelineJob("ONO/Front End/FE Smoke Tests - Staging/Analytics - AMP Article - Fe
     }
   }
 }
+pipelineJob("ONO/Front End/FE Smoke Tests - Preproduction behind Akamai/Analytics - AMP Article - Features - Features - LG5-5201") {
+  definition {
+    cps {
+      script('''
+        pipeline {
+          agent any
+          stages {
+            stage('Build'){
+                steps{
+                    sh 'COMPONENT=MT/AMPArticles/Analytics/Features TESTCASE=SmokeTestMTFeaturesAMPArticle npm run devtools-kubernetes'
+                }
+            }
+          }
+        }
+    ''')   
+    }
+  }
+}
+pipelineJob("ONO/Front End/FE Smoke Tests - Production/Analytics - AMP Article - Features - Features - LG5-5201") {
+  definition {
+    cps {
+      script('''
+        pipeline {
+          agent any
+          stages {
+            stage('Build'){
+                steps{
+                    sh 'COMPONENT=MT/AMPArticles/Analytics/Features TESTCASE=SmokeTestMTFeaturesAMPArticle npm run devtools-kubernetes'
+                }
+            }
+          }
+        }
+    ''')   
+    }
+  }
+}
