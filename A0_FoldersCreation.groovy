7642f91bba4816a8c7e834a3a10aa929256f91d8
Gilberto Sanchez Mares <69218351+GSanchezMTD@users.noreply.github.com>
2022-07-14 16:16:41 -0500

    Update A0_FoldersCreation.groovy

--- A0_FoldersCreation.groovy
+++ A0_FoldersCreation.groovy
@@ -1,4 +1,32 @@
-folder('Smoke Tests'){
+folder('ONO'){
   description('my description')
-  displayName('Smoke Tests')
+  displayName('ONO')
+}
+folder('/ONO/Front End'){
+  description('my description')
+  displayName('Front End')
+}
+folder('/ONO/Front End/FE Smoke Tests - Staging'){
+  description('my description')
+  displayName('FE Smoke Tests - Staging')
+}
+folder('/ONO/Front End/FE Smoke Tests - Production'){
+  description('my description')
+  displayName('FE Smoke Tests - Production')
+}
+folder('/ONO/Front End/FE Smoke Tests - Preproduction behind Akamai'){
+  description('my description')
+  displayName('FE Smoke Tests - Preproduction behind Akamai')
+}
+folder('/ONO/Front End/FE Regression Tests - Staging'){
+  description('my description')
+  displayName('FE Regression Tests - Staging')
+}
+folder('/ONO/Front End/FE Regression Tests - Production'){
+  description('my description')
+  displayName('FE Regression Tests - Production')
+}
+folder('/ONO/Front End/FE Regression Tests - Preproduction Behind Akamai'){
+  description('my description')
+  displayName('FE Regression Tests - Preproduction Behind Akamai')
 }
